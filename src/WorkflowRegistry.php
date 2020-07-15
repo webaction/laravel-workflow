9b34400e949c84b7bec36ee7730fbea18f7d40b0
Mischa <mischa@webaction.com.au>
2020-07-15 14:49:03 +1000

    Update WorkflowRegistry.php

--- src/WorkflowRegistry.php
+++ src/WorkflowRegistry.php
@@ -14,6 +14,7 @@ use Symfony\Component\Workflow\StateMachine;
 use Symfony\Component\Workflow\SupportStrategy\InstanceOfSupportStrategy;
 use Symfony\Component\Workflow\Transition;
 use Symfony\Component\Workflow\Workflow;
+use Symfony\Component\Workflow\Metadata\InMemoryMetadataStore;
 
 /**
  * @author Boris Koumondji <brexis@yahoo.fr>
